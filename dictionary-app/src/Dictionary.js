3f31058d1fef9008805440e0de0654c5fa8fd6b2
JazmineOuterbridge <108620390+JazmineOuterbridge@users.noreply.github.com>
2022-09-12 19:06:22 -0300

    added axios

--- dictionary-app/src/Dictionary.js
+++ dictionary-app/src/Dictionary.js
@@ -1,15 +1,25 @@
 import React, {useState} from "react";
 import './Dictionary.css';
+import axios from "axios";
 
 export default function Dictionary(){
-    let {keyword, setkeyword} = useState("");
+    let {keyword, setKeyword} = useState("");
+   
+   function handleRespose(){
+console.log(response.data[0]);
+   }
+
     function search(event){
-        event.preventDefault();
-    alert(`Searching for ${keyword}`);
+     event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleRespose);
     }
     
+
+
 function handleKeywordChange(event){
-    setkeyword(event.target.value);
+    setKeyword(event.target.value);
 }
 
     return <div className="Dictionary">
