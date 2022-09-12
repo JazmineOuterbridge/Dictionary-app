299712d81a3dc6604b52cb78d80a52c3414d97a6
JazmineOuterbridge <108620390+JazmineOuterbridge@users.noreply.github.com>
2022-09-12 18:52:28 -0300

    created search

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-
+import Dictionary from "./Dictionary"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="container">
       <h1>dictionary-app</h1>
-      <a href="a" className="btn btn-primary">Button</a>
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer>Coded by Jazmine Outerbridge</footer>
     </div>
   );
 }
